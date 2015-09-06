37bf8b8b2971c3f4d81533ee1f4aae685d5cb74d
George Chen (Air) <sylvanknave@gmail.com>
2015-09-06 13:20:35 -0500

    Finished up example of retrieving datasets

--- srdb-data-filer.js
+++ srdb-data-filer.js
@@ -3,31 +3,45 @@ var path = require('path');
 var Filer = require('./filer');
 
 var p = '/Users/knav/github/srd-vault/www/data/';
-var p = '/Users/knav/github/codex/';
+
+var eligible = [
+  'srd111_NIST_Atomic_Ionization_Energies_Output.json',
+  'srd144_Atomic_Weights_and_Isotopic_Compositions_for_All_Elements.json',
+  'srd13_janaf.species.json',
+  'srd121_allascii_2014.json'
+]
 
 
 var srd = {};
 
 var filter = function (fileContent, fromPath, toPath) {
 
-  var json = '';
 
-  try {
-    json = JSON.parse(fileContent);
+  var fromDirectory = path.dirname(fromPath); // data/srd121/f.json -> data/srd121
+  var fileInfo = path.parse(fromDirectory);
+  var srdNumber = fileInfo.base; // data/srd121 -> srd121
+  srdNumber = srdNumber.replace(/\D/g, ''); // srd121 -> 121
+
+
+  var fileName = path.basename(fromPath);
+
+  if (-1 !== eligible.indexOf(fileName)) {
+    console.log('Appending ' + fromPath);
+
+    var json = '';
+    try {
+      json = JSON.parse(fileContent);
+    }
+    catch (exception) {
+      console.error('Could not parse ' + fromPath);
+      console.error(exception);
+    }
+
+    if (json) {
+      srd[srdNumber] = json;
+    }
   }
-  catch (exception) {
-    console.error('Could not parse ' + fileContent);
-  }
 
-  if (json) {
-
-    var fromDirectory = path.dirname(fromPath);
-    var srdNumber = path.parse(fromDirectory).base;
-
-    srdNumber = srdNumber.replace(/\D/g, ''); // srd121 -> 121
-
-    srd[srdNumber] = json;
-  }
   return null; // null == do not save file
 };
 
@@ -45,5 +59,8 @@ Filer.filterFileContent(
 
 // Once we've built a giant SRD, stringify it and save it as a factory.js
 
-// console.log(srd);
+output = JSON.stringify(srd);
+console.log(output.substr(0, 300));
+
+Filer.saveFile('srd.js', output);
 
